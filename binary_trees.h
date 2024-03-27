b3fd8e73dfcf35aa0de3b9003bc8ac17d1126068
Emmanuel <kcemmy03@gmail.com>
2024-03-27 03:35:14 +0100

    Added prototype for function

--- binary_trees.h
+++ binary_trees.h
@@ -4,6 +4,7 @@
 /* Standard library header files */
 #include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -35,5 +36,5 @@ typedef struct binary_tree_s heap_t;
 /* Function Prototypes */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 #endif /* _BINARY_TREES_H_ */
