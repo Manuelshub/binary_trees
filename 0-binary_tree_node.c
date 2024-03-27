82a9a63ffa22a22c818f54c86cd89303f5d80285
Emmanuel <kcemmy03@gmail.com>
2024-03-27 03:39:00 +0100

    Fix betty

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,8 +2,8 @@
 
 /**
  * binary_tree_node - creates a binary tree node.
- * parent: pointer to the parent node of the node to create.
- * value: value to put in the new node.
+ * @parent: pointer to the parent node of the node to create.
+ * @value: value to put in the new node.
  *
  * Return: a pointer to the new node or NULL on failure.
  */
