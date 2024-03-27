b0e93965f930595ca47ea9464b85096a2555ea08
Emmanuel <kcemmy03@gmail.com>
2024-03-27 03:48:23 +0100

    Initialized the left and right members

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,6 +20,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	new->n = value;
 	new->parent = parent;
+	new->right = NULL;
+	new->left = NULL;
 	if (parent->left)
 	{
 		new->left = parent->left;
